534f57e3922c19f38c60e035ec07f921edd5c696
Ben Marquez <bmarquez1997@gmail.com>
2019-04-30 00:00:52 -0400

    added copypastas, help script, and 2 other command

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+var fs = require('fs');
+var copyPastas = ['gayCat.txt', 'mood.txt', 'steveHarvey.txt', 'stuartLittle.txt', 'navySeal.txt'];
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -31,11 +33,29 @@ bot.on('message', function (user, userID, channelID, message, evt) {
        
         args = args.splice(1);
         switch(cmd) {
-            // !ping
+            case 'help':
+                bot.sendMessage({
+                    to: channelID,
+                    message: fs.readFileSync('./helpScript.txt', 'utf-8')
+                });
+            break;
             case 'shitpost':
                 bot.sendMessage({
                     to: channelID,
                     message: 'no u'
+                });
+            break;
+            case 'copypasta':
+                luckyNumber = Math.floor(Math.random() * 5);
+                bot.sendMessage({
+                    to: channelID,
+                    message: fs.readFileSync('./copypastas/' + copyPastas[luckyNumber], 'utf-8')
+                });
+            break;
+            case 'meme':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'still working on it m8, check back later'
                 });
             break;
          }
